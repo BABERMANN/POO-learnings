b656a2ce6be4b514863ba4b3d5f7355515262b25
Caua Matheus Habermann Pereira <cauahabermann7@gmail.com>
2026-09-25 14:50:59 -0300

    chore: add educational comments

--- lesson-5-to/lesson05-ex12-protected-access/src/main/java/org/example/Instrumento.java
+++ lesson-5-to/lesson05-ex12-protected-access/src/main/java/org/example/Instrumento.java
@@ -1,7 +1,7 @@
 package org.example;
 
 public class Instrumento {
-    protected String tipo;
+    protected String tipo; // protected mais permissivo deixa classes que herdam de instrumento acessar tipo sem ter que usar o getter
 
     public Instrumento(String tipo) {
         this.tipo = tipo;
